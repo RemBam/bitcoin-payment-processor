e22963cfa27c3fe305c91ac8609b10cef0eb0b04
RemBam <przychodzen.j@gmail.com>
2015-02-20 22:59:38 +0100

    Add PaymentNotificationActor::notify

--- src/main/scala/com/wlangiewicz/bitcoinpaymentprocessor/actors/PaymentNotificationActor.scala
+++ src/main/scala/com/wlangiewicz/bitcoinpaymentprocessor/actors/PaymentNotificationActor.scala
@@ -1,15 +1,36 @@
 package com.wlangiewicz.bitcoinpaymentprocessor.actors
 
-import akka.actor.Actor
+import akka.actor.{ActorSystem, Actor}
 import akka.actor.Actor.Receive
-import com.wlangiewicz.bitcoinpaymentprocessor.PaymentCompletedResponse
+import com.wlangiewicz.bitcoinpaymentprocessor.{JsonFormats, PaymentCompletedResponse}
+import com.wlangiewicz.bitcoinpaymentprocessor.actors.PaymentNotificationActor.NotifyPaid
+import spray.http._
+import spray.client.pipelining._
+import spray.httpx.SprayJsonSupport._
 
-object PaymentNotificationActor {
+import scala.concurrent.Future
+
+object PaymentNotificationActor extends JsonFormats{
 
   case class NotifyPaid(notificationUrl: String, payload: PaymentCompletedResponse)
 
+  def notify(url: String, payload: PaymentCompletedResponse): Unit = {
+    implicit val system = ActorSystem()
+    import system.dispatcher  // execution context for futures
+
+    val pipeline: HttpRequest => Future[HttpResponse] = sendReceive
+
+    val response: Future[HttpResponse] = pipeline(Post(url, payload))
+    Console.println(response)
+  }
 }
 
-class PaymentNotificationActor extends Actor {
-  override def receive: Receive = ???
+class PaymentNotificationActor extends Actor with JsonFormats{
+
+  override def receive = {
+    case NotifyPaid(n, p) => {
+      PaymentNotificationActor.notify(n,p)
+    }
+
+  }
 }
