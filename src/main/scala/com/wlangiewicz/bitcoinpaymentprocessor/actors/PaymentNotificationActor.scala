d1a68940bf4dcbe909b095cb5f7a90969a506bb4
RemBam <przychodzen.j@gmail.com>
2015-02-21 06:29:44 +0100

    Add msg replay with increasing delay

--- src/main/scala/com/wlangiewicz/bitcoinpaymentprocessor/actors/PaymentNotificationActor.scala
+++ src/main/scala/com/wlangiewicz/bitcoinpaymentprocessor/actors/PaymentNotificationActor.scala
@@ -1,36 +1,48 @@
 package com.wlangiewicz.bitcoinpaymentprocessor.actors
 
-import akka.actor.{ActorSystem, Actor}
+import akka.actor.{Props, ActorSystem, Actor}
 import akka.actor.Actor.Receive
 import com.wlangiewicz.bitcoinpaymentprocessor.{JsonFormats, PaymentCompletedResponse}
-import com.wlangiewicz.bitcoinpaymentprocessor.actors.PaymentNotificationActor.NotifyPaid
+import com.wlangiewicz.bitcoinpaymentprocessor.actors.PaymentNotificationActor.{DelayedNotifyPaid, NotifyPaid}
 import spray.http._
 import spray.client.pipelining._
 import spray.httpx.SprayJsonSupport._
 
 import scala.concurrent.Future
+import scala.language.postfixOps
+import scala.util.{Success, Failure}
+import scala.concurrent.duration._
 
-object PaymentNotificationActor extends JsonFormats{
+object PaymentNotificationActor {
 
   case class NotifyPaid(notificationUrl: String, payload: PaymentCompletedResponse)
+  case class DelayedNotifyPaid(notificationUrl: String, payload: PaymentCompletedResponse, delay: Int)
+}
 
-  def notify(url: String, payload: PaymentCompletedResponse): Unit = {
-    implicit val system = ActorSystem()
-    import system.dispatcher  // execution context for futures
+class PaymentNotificationActor extends Actor with JsonFormats {
 
+  import scala.concurrent.ExecutionContext.Implicits.global
+
+  def notify(url: String, payload: PaymentCompletedResponse, delay: Int = 1): Unit = {
     val pipeline: HttpRequest => Future[HttpResponse] = sendReceive
 
     val response: Future[HttpResponse] = pipeline(Post(url, payload))
-    Console.println(response)
-  }
-}
+    response.onComplete{
+      case Failure(ex) => {
+        implicit val system = ActorSystem("payment-processor")
 
-class PaymentNotificationActor extends Actor with JsonFormats{
+        val paymentActor = system.actorOf(Props[PaymentNotificationActor], "notify")
+
+        system.scheduler.scheduleOnce(delay seconds, paymentActor, DelayedNotifyPaid(url,payload,delay*2))
+      }
+    }
+  }
+
 
   override def receive = {
-    case NotifyPaid(n, p) => {
-      PaymentNotificationActor.notify(n,p)
-    }
-
+    case NotifyPaid(n, p) =>
+      notify(n, p)
+    case DelayedNotifyPaid(n, p, t) =>
+      notify(n, p, t)
   }
 }
